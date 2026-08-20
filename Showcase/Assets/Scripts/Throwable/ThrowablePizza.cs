367627abbcd580efa9deaf619c6c57199833b9ca
Ali <alireza2.moazami@live.uwe.ac.uk>
2026-08-20 06:09:00 +0100

    Building models and started work on throwable pizza script
    + New MainMenu Level
    + Throwable Pizza script
    + Building models
    + Temp red pizza material for object
    + New folders for layout

---
+++ Showcase/Assets/Scripts/Throwable/ThrowablePizza.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ThrowablePizza : MonoBehaviour
+{
+    private float PizzaRotationSpeed = 200;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, PizzaRotationSpeed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "DeliverySlot")
+        {
+            Destroy(this);
+        }
+    }
+}
