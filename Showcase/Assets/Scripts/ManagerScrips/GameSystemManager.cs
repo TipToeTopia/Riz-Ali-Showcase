b97f707e7c643cf111ae64955ba3f715b7bce91a
Ali <alireza2.moazami@live.uwe.ac.uk>
2026-08-23 06:16:25 +0100

    Implementation added for Tip Notification after delivery + adjusted some variable names
    + TMPTextPro to project
    + New materials for the building prefab
    + Added comments to scripts
    
    Should the game system manager handle all these ?

--- Showcase/Assets/Scripts/ManagerScrips/GameSystemManager.cs
+++ Showcase/Assets/Scripts/ManagerScrips/GameSystemManager.cs
@@ -1,5 +1,8 @@
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 
 public class GameSystemManager : MonoBehaviour
 {
@@ -7,40 +10,84 @@ public class GameSystemManager : MonoBehaviour
     [Header("Delivery Slot Data")]
     [SerializeField] List<GameObject> DeliverySlots;
     [SerializeField] int SlotIndex;
-    [SerializeField] GameObject NextActiveSlot;
+    [SerializeField] GameObject NextActiveDelivery;
 
-    private int DeliveryCash = 15;
-    private int DeliveryTips;
+    [Header("Cash and Tips Data")]
+    [SerializeField] int DeliveryCash = 15;
+    [SerializeField] int DeliveryTips;
+    public TextMeshProUGUI TipsNotifiText;
+    [SerializeField] float TipsNotifiFadeTime;
+
+    [Header("Game Timer")]
+    public TextMeshProUGUI GameTimerText;
+    [SerializeField] float CurrentGameTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Selecting the first delivery slot to activate
         DeliverySlots = new List<GameObject>();
         DeliverySlots.AddRange(GameObject.FindGameObjectsWithTag("DeliverySlot"));
-
         SlotIndex = Random.Range(0, DeliverySlots.Count);
-
         GameObject FirstDeliverySlot = DeliverySlots[SlotIndex];
         FirstDeliverySlot.GetComponent<DeliverySlot>().UpdateDeliveryStatus(true);
     }
 
+    void Update()
+    {
+        //Game Timer Countdown
+        CurrentGameTime -= Time.deltaTime;
+        int timerMinutes = Mathf.FloorToInt(CurrentGameTime / 60);
+        int timerSeconds = Mathf.FloorToInt(CurrentGameTime % 60);
+        GameTimerText.text = string.Format("{0:00}:{1:00}", timerMinutes, timerSeconds);
+
+        if(CurrentGameTime < 1)
+        {
+            //End the game and move to results scene
+            Debug.Log("Shift Over");
+        }
+    }
+
     public void NextDelivery(GameObject PreviousDelivery)
     {
+        //Preventing the previous slot from being selected again in the list
+        //Selecting the next delivery slot
         DeliverySlots.Remove(PreviousDelivery);
-
         SlotIndex = Random.Range(0, DeliverySlots.Count);
-
-        NextActiveSlot = DeliverySlots[SlotIndex];
-        NextActiveSlot.GetComponent<DeliverySlot>().UpdateDeliveryStatus(true);
+        NextActiveDelivery = DeliverySlots[SlotIndex];
+        NextActiveDelivery.GetComponent<DeliverySlot>().UpdateDeliveryStatus(true);
         GiveCashAndTips();
-
         DeliverySlots.Add(PreviousDelivery);
     }
 
     void GiveCashAndTips()
     {
-        DeliveryTips = Random.Range(0, 15);
+        //SHOULD MAKE TIP VALUE BE BASED ON HOW QUICK PLAYER DELIVERS
+
+        //Currently getting a random int for the tip
+        DeliveryTips = Random.Range(1, 15);
         Debug.Log("You got $" + DeliveryCash + " for a delivery!");
         Debug.Log("You got $" + DeliveryTips + " as a tip!");
+
+        StartCoroutine(TipsNotifi());
     }
+
+    IEnumerator TipsNotifi()
+    {
+        //Tip notification text fade out
+        TipsNotifiText.enabled = true;
+        TipsNotifiText.text = ("You got a $" + DeliveryTips + " tip!");
+
+        while (TipsNotifiText.color.a > 0f)
+        { 
+            TipsNotifiText.color = new Color(255, 255, 255, TipsNotifiText.color.a - Time.deltaTime / TipsNotifiFadeTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+        TipsNotifiText.enabled = false;
+        TipsNotifiText.color = new Color32(255, 255, 255, 255);
+    }
+
+
 }
